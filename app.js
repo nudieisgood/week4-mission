8c86bd2bc537d99d4829567ca5d2abac4cee0818
nudieisgood <nudieisgoodmii@gmail.com>
2024-01-06 12:06:02 +0800

    first

---
+++ app.js
@@ -0,0 +1,14 @@
+import {
+  createApp,
+  ref,
+} from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
+// import { data } from "./data.js";
+
+const App = {
+  setup() {
+    return {};
+  },
+};
+
+const app = createApp(App);
+app.mount("#app");
